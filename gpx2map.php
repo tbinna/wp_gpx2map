e5d29f72d0008e1a1caa1631257346bd0723b724
Tobias Binna <tobi.binna@gmail.com>
2014-11-07 15:33:21 +0800

    Added trail title field and metabox

--- gpx2map.php
+++ gpx2map.php
@@ -24,7 +24,7 @@ define( 'AT_GPX2MAP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 /**
  * Enqueue scripts and styles
  */
-function blog_post_map_scripts() {
+function gpx2map_scripts() {
 
 	global $wp_styles;
 
@@ -51,70 +51,96 @@ function blog_post_map_scripts() {
 	wp_enqueue_script('mapbox marker cluster', 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js', array('mapbox'), '0.4.0', false );
 }
 
-add_action('wp_enqueue_scripts', 'blog_post_map_scripts');
+add_action('wp_enqueue_scripts', 'gpx2map_scripts');
 
+/* Fire our meta box setup function on the post editor screen. */
+add_action( 'load-post.php', 'gpx2map_meta_boxes_setup' );
+add_action( 'load-post-new.php', 'gpx2map_meta_boxes_setup' );
 
-class Marker {
+function gpx2map_meta_boxes_setup() {
+	/* Add meta boxes on the 'add_meta_boxes' hook. */
+	add_action( 'add_meta_boxes', 'gpx2map_add_post_meta_boxes' );
 
-	var $title;
-	var $permalink;
-	var $latLon;
-	var $postThumbnailUrl;
-
-	function Marker($title, $permalink, $latLon, $postThumbnailUrl) {
-       $this->title = $title;
-       $this->permalink = $permalink;
-       $this->latLon = $latLon;
-       $this->postThumbnailUrl = $postThumbnailUrl;
-   }
+	/* Save post meta on the 'save_post' hook. */
+	add_action( 'save_post', 'gpx2map_save_post_class_meta', 10, 2 );
 }
 
+function gpx2map_add_post_meta_boxes() {
+	add_meta_box('gpx2map-trail', esc_html__('GPX2Map', 'gpx2map'), 'gpx2map_post_class_meta_box', 'post', 'normal', 'default');
+}
 
-/** Create and return map */
-function getMap() {
+/* Display the post meta box. */
+function gpx2map_post_class_meta_box($object, $box) { ?>
 
-	global $wp_query, $mygpGeotagsGeoMetatags_key;
+  <?php wp_nonce_field( basename( __FILE__ ), 'gpx2map_trail_nonce' ); ?>
 
-	$markers = array();
-	$posts = get_posts(array('numberposts' => 1000, 'meta_key' => $mygpGeotagsGeoMetatags_key, 'post_status' => 'publish'));
+  <p>
+    <label for="gpx2map-trail-title"><?php _e( "Trail title", 'gpx2map' ); ?></label>
+    <br />
+    <input class="widefat" type="text" name="gpx2map-trail-title" id="gpx2map-trail-title" size="30" />
+  </p>
+<?php }
 
-	foreach($posts as $post) {
-		$positionData = get_post_meta($post->ID, $mygpGeotagsGeoMetatags_key, true);
+/* Save the meta box's post metadata. */
+function gpx2map_save_post_class_meta( $post_id, $post ) {
 
-		$dataSplitted = "";
-		if($positionData['position'] != "") {
-			$dataSplitted = array_map('doubleval', explode(";", $positionData[ 'position' ]));
-		} else {
-			continue; // post seems not to be georeferenced
-		}
+  /* Verify the nonce before proceeding. */
+  if ( !isset( $_POST['gpx2map_trail_nonce'] ) || !wp_verify_nonce( $_POST['gpx2map_trail_nonce'], basename( __FILE__ ) ) )
+    return $post_id;
 
-		$postThumbnailUrl = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+  /* Get the post type object. */
+  $post_type = get_post_type_object( $post->post_type );
 
-		array_push($markers, new Marker(get_the_title($post->ID), get_permalink($post->ID), $dataSplitted, $postThumbnailUrl));
+  /* Check if the current user has permission to edit the post. */
+  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+    return $post_id;
+
+  /* Get the posted data and sanitize it for use as an HTML class. */
+  $new_meta_value = ( isset( $_POST['gpx2map-trail-title'] ) ? sanitize_html_class( $_POST['gpx2map-trail-title'] ) : '' );
+
+  /* Get the meta key. */
+  $meta_key = 'gpx2map-trail';
+
+  /* Get the meta value of the custom field key. */
+  $meta_value = get_post_meta( $post_id, $meta_key, true );
+
+  /* If a new meta value was added and there was no previous value, add it. */
+  if ( $new_meta_value && '' == $meta_value )
+    add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+
+  /* If the new meta value does not match the old value, update it. */
+  elseif ( $new_meta_value && $new_meta_value != $meta_value )
+    update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+  /* If there is no new meta value but an old value exists, delete it. */
+  elseif ( '' == $new_meta_value && $meta_value )
+    delete_post_meta( $post_id, $meta_key, $meta_value );
+}
+
+function gpx2map_trail_info_html() {
+	/* Get the custom post class. */
+    $trail_title = get_post_meta( $post_id, 'gpx2map-trail-title', true );
+
+	$html = '<div id="gpx2map-trail-title">' . $trail_title . '</div>';
+
+	retrun $html;
+}
+
+/* Filter the post class hook with our custom post class function. */
+add_filter( 'the_content', 'gpx2map_add_trail_info' )
+
+function gpx2map_add_trail_info($content) {
+	* Get the current post ID. */
+  	$post_id = get_the_ID();
+
+  	if ( !empty( $post_id ) ) {
+  		$shortcode = '[at_gpx2map]';
+
+		$html = gpx2map_trail_info_html();
+		$content = str_replace($shortcode, $html, $content);
 	}
 
-	$mapId = 'map' . $wp_query->post->ID;
-
-	$html = '<div id="' . $mapId . '" class="blog-post-map"></div>';
-	$html .= '<script type="text/javascript">initMap(' . $mapId . ', ' . json_encode($markers) .');</script>';
-
-	return $html;
-	
+	return $content;
 }
-
-/** Add map to post */
-function addMap($content) {
-
-	global $wp_query;
-	
-	$postId = $wp_query->post->ID;
-	$shortcode = '[at_gpx2map]';
-
-	$html = getMap();
-	$content = str_replace($shortcode, $html, $content);
-
-    return $content;
-}
-add_filter('the_content', 'addMap');
 
 ?>
