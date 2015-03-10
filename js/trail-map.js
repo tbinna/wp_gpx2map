7f1aa8bb7677fc36a873bed3962dcffa78172d38
Tobias Binna <tobi.binna@gmail.com>
2015-03-10 18:15:50 +0800

    Updated map tile layer to use mapbox

--- js/trail-map.js
+++ js/trail-map.js
@@ -1,6 +1,6 @@
 var map = L.map('trail-map').setView([51.505, -0.09], 13);
 
-L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
+L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoidGJpbm5hIiwiYSI6InpqcW8tT1kifQ.bTs8e9ttuX0jGmNpGobTqg', {
 	attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 }).addTo(map);
 
